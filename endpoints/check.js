c5fc79b92c02e8a4dcd2094b20b0bfecc544feef
Xenorio <realxenorio@gmail.com>
2022-02-14 23:12:55 +0100

    Fix domain filter

--- endpoints/check.js
+++ endpoints/check.js
@@ -3,17 +3,17 @@ module.exports.get = async(req, res) => {
         error: 'No domain provided'
     })
 
-    let domain = req.query.domain
+    let domain = req.query.domain.toLowerCase()
 
     // Check local domains
-    let localEntry = process.localDomains.find(x => domain.endsWith(x.domain))
+    let localEntry = process.localDomains.find(x => domain == x.domain || domain.endsWith('.' + x.domain))
     if (localEntry) {
         res.json({
             blocked: true,
             reason: localEntry.reason || 'Not provided',
             timestamp: localEntry.timestamp
         })
-    } else if (process.externalDomains.find(x => domain.endsWith(x))) { // Check external domains
+    } else if (process.externalDomains.find(x => domain == x || domain.endsWith('.' + x))) { // Check external domains
         res.json({
             blocked: true,
             reason: 'Checked externally'
